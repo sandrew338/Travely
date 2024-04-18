e983011783a003ae484902be5f0c1400fa558e5d
sandrew338 <andrey.nazarkevych@gmail.com>
2024-04-18 18:49:36 +0300

    1

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,4 +1,8 @@
+import 'dart:js_interop';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -8,9 +12,10 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-    body: StreamBuilder<User?>(stream: Firebase,),
+    return const Scaffold(
+    body: StreamBuilder(firebase_auth.instanceof(constructor))
     )
+    
+    
   }
 }
