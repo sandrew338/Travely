38fc02f87c2dca62790102da9f1f3d71da26f98f
sandrew338 <andrey.nazarkevych@gmail.com>
2024-04-18 23:11:40 +0300

    1

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,20 +1,17 @@
-import 'dart:js_interop';
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-    body: StreamBuilder(firebase_auth.instanceof(constructor))
-    )
+    return Scaffold(
+    body: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder:context, snapshot),)
+  
+      );
     
     
   }
