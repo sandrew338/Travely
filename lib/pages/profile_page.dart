ff3867483628fa7739c970652d4b84942ce11ee0
ivan-ivano <ivan.ivano.ri.2023@lpnu.ua>
2024-04-25 14:35:20 +0300

    authentifiucation

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,18 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+class ProfilePage extends StatelessWidget {
+  ProfilePage({super.key});
 
   @override
-  State<ProfilePage> createState() => _ProfilePageState();
-}
+  final user = FirebaseAuth.instance.currentUser;
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
 
-class _ProfilePageState extends State<ProfilePage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title:const Text("ProfilePage")),
-    body:SvgPicture.asset("assets/images/selected/time_past.svg"),
-    );
+    return Scaffold(
+        appBar: AppBar(title: const Text("ProfilePage")),
+        body: Text("Logged in as ${user!.email}"),
+        floatingActionButton:
+            IconButton(onPressed: signUserOut, icon: const Icon(Icons.logout)));
+    //SvgPicture.asset("assets/images/selected/time_past.svg")
   }
 }
