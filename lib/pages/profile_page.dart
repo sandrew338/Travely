908eb125ad88e7c0001c3541978ee53584152715
sandrew338 <andrey.nazarkevych@gmail.com>
2024-05-01 10:06:58 +0300

    1234

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:travely/components/text_box.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -9,54 +11,108 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  //user
-  //final currentUser = FirebaseAuth.instance.currentUser!;
+  final currentUser = FirebaseAuth.instance.currentUser;
 
-void signUserOut() {
+  Future<void> editField(String field) async {}
+
+  void signUserOut() {
     FirebaseAuth.instance.signOut();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      appBar: AppBar(
-        actions: [
-          IconButton(onPressed: signUserOut, icon: const Icon(Icons.logout))
-        ],
-        title: const Text("ProfilePage"),
-        backgroundColor: Colors.grey[900],
-      ),
-      body: ListView(
-        children:  [
-          const SizedBox(height: 50),
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          actions: [
+            IconButton(onPressed: signUserOut, icon: const Icon(Icons.logout))
+          ],
+          title: const Text("ProfilePage"),
+          backgroundColor: Colors.grey[900],
+        ),
+        body: StreamBuilder<DocumentSnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection("Users")
+                .doc(currentUser?.uid)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final userData = snapshot.data!.data() as Map<String, dynamic>;
 
-          //profile pic
-          const Icon(Icons.person,
-          size: 72,),
+                return ListView(
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
 
+                    //profile pic
 
-          //user email
-          Text("currentUser Email",
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.grey[700],)),
+                    const Icon(
+                      Icons.person,
+                      size: 100,
+                    ),
 
-          const SizedBox(height: 50),
+                    const SizedBox(
+                      height: 50,
+                    ),
 
-          //user details
+                    //user email
+                    Text(currentUser!.email!,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.grey[700])),
 
-          Padding(padding: const EdgeInsets.only(left: 25.0),
-          child: Text(
-            "My details",
-            style: TextStyle(color: Colors.grey[600]),
-          ))
-          //username
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    //user details
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25.0),
+                      child: Text("My Details",
+                          style: TextStyle(color: Colors.grey[600])),
+                    ),
 
-          //bio
+                    //username
 
-          //users routs
+                    MyTextBox(
+                      sectionName: userData["username"],
+                      text: "dfdsf",
+                      onPressed: () => editField("username"),
+                    ),
 
-        ],
-      ),
-    );
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    //bio
+
+                    MyTextBox(
+                      sectionName: userData["bio"],
+                      text: "bio",
+                      onPressed: () => editField("bio"),
+                    ),
+
+                    //user posts
+
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25.0),
+                      child: Text("My Posts",
+                          style: TextStyle(color: Colors.grey[600])),
+                    ),
+
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text("Error${snapshot.error}"),
+                );
+              }
+
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            })); //SvgPicture.asset("assets/images/selected/time_past.svg")
   }
 }
